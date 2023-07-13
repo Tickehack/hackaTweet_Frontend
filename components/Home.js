579a0ce740c7ecf3b6232fac8cb5918b1e526dd8
Benjamin Saragaglia <saragaglia.benjamin@gmail.com>
2023-07-13 23:04:09 +0200

    surtout premières modifs d'_app

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,43 @@
 import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Login from './Login';
+
 
 function Home() {
-  return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
-    </div>
-  );
+  const [homepage, setHomepage] = useState(true);
+
+  const changedepage = () => {
+    setHomepage(!homepage)
+  }
+
+
+  if (homepage){
+    return (
+      <div><button onClick={()=> changedepage()}> Change de page</button>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            <Login />
+          </h1>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <main className={styles.main}><button onClick={()=> changedepage()}> Change de page</button>
+          <h1 className={styles.title}>
+            Messages
+          </h1>
+        </main>
+      </div>
+    );
+  }
+
+
+
+
+  
 }
 
 export default Home;
